57999a4b050c4d6ac083710c1f753a100ceea35d
danikiy <danikiy@hotmail.com>
2021-04-18 14:26:57 +0100

    Create standardclasses.js

---
+++ StandardDemo/standardclasses.js
@@ -0,0 +1,14 @@
+var standardClasses = {
+ 0: 'Activities',
+ 1: 'Architecture',
+ 2: 'Cities',
+ 3: 'Clothing',
+ 4: 'Dates',
+ 5: 'Dessert',
+ 6: 'Drinks',
+ 7: 'Food',
+ 8: 'Instruments',
+ 9: 'Nature',
+ 10: 'Religious Sites',
+ 11: 'Souq'
+};
